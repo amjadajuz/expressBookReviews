88b115a1fc94ad0b82eaacc886806c6a771992f9
chethas-qb <chethas@qburst.com>
2025-07-26 13:03:08 +0530

    Implement user registration and login functionality in auth_users.js and general.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,16 +7,39 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+return users.some(user => user.username === username);
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+const user = users.find(user => user.username === username && user.password === password);
+if(user){
+  return true;
+}else{
+  return false;
+}
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if(username && password){
+    if(authenticatedUser(username,password)){
+      //generate token
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 * 60 });
+      req.session.authorization = {
+        accessToken,username
+      }
+      return res.status(200).json({message: "User successfully logged in",accessToken: accessToken});
+    }else{
+      return res.status(401).json({message: "Invalid username or password"});
+    }
+  }
+  return res.status(404).json({message: "User not found"});
 });
 
 // Add a book review
