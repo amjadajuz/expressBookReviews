2aae3de32a2c00150a5af1ecbca93e0f2826682b
chethas-qb <chethas@qburst.com>
2025-07-26 20:10:11 +0530

    Refactor book retrieval endpoints in general.js to use Promises for improved error handling and response management.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,55 +24,81 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(200).json(books);
+  Promise.resolve(books)
+    .then((bookList) => {
+      return res.status(200).json(bookList);
+    })
+    .catch((error) => {
+      return res.status(500).json({message: "Error retrieving books", error: error.message});
+    });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  const book = books[isbn];
-  if(book){
-    return res.status(200).json(book);
-  }else{
-    return res.status(404).json({message: "Book not found"});
-  }
+  Promise.resolve(books[isbn])
+    .then((book) => {
+      if(book){
+        return res.status(200).json(book);
+      }else{
+        return res.status(404).json({message: "Book not found"});
+      }
+    })
+    .catch((error) => {
+      return res.status(500).json({message: "Error retrieving book", error: error.message});
+    });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   const author = req.params.author;
-  const booksByAuthor = Object.values(books).filter(book => book.author === author);
-  if(booksByAuthor.length > 0){
-    return res.status(200).json(booksByAuthor);
-  }else{
-    return res.status(404).json({message: "No books found by this author"});
-  }
+  Promise.resolve(Object.values(books).filter(book => book.author === author))
+    .then((booksByAuthor) => {
+      if(booksByAuthor.length > 0){
+        return res.status(200).json(booksByAuthor);
+      }else{
+        return res.status(404).json({message: "No books found by this author"});
+      }
+    })
+    .catch((error) => {
+      return res.status(500).json({message: "Error retrieving books by author", error: error.message});
+    });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
   const title = req.params.title;
-  const booksByTitle = Object.values(books).filter(book => book.title === title);
-  if(booksByTitle.length > 0){
-    return res.status(200).json(booksByTitle);
-  }else{
-    return res.status(404).json({message: "No books found with this title"});
-  }
+  Promise.resolve(Object.values(books).filter(book => book.title === title))
+    .then((booksByTitle) => {
+      if(booksByTitle.length > 0){
+        return res.status(200).json(booksByTitle);
+      }else{
+        return res.status(404).json({message: "No books found with this title"});
+      }
+    })
+    .catch((error) => {
+      return res.status(500).json({message: "Error retrieving books by title", error: error.message});
+    });
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  const book = books[isbn];
-  if(book){
-    return res.status(200).json(book.reviews);
-  }else{
-    return res.status(404).json({message: "Book not found"});
-  }
+  Promise.resolve(books[isbn])
+    .then((book) => {
+      if(book){
+        return res.status(200).json(book.reviews);
+      }else{
+        return res.status(404).json({message: "Book not found"});
+      }
+    })
+    .catch((error) => {
+      return res.status(500).json({message: "Error retrieving book reviews", error: error.message});
+    });
 });
 
 module.exports.general = public_users;
